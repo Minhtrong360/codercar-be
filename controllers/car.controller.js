cac9b4a002e8da1ecea334cfb1105292566bebcb
Minhtrong360 <nguyenminhtrong402@gmail.com>
2022-12-22 18:37:47 +0700

    Can't get documents from Mongodb Compass

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,93 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
+const { sendResponse, AppError } = require("../helpers/utils.js");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const info = req.body;
+  try {
+    // YOUR CODE HERE
+    if (!info) throw new AppError(402, "Bad Request", "Create Car Error");
+    const created = await Car.create(info);
+    sendResponse(res, 200, true, { car: created }, null, "Create Car Success");
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const filter = req.query ? req.query : {};
+
+  try {
+    // YOUR CODE HERE
+    //mongoose query
+    const listOfFound = await Car.find(filter);
+    console.log("first", listOfFound.length);
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: listOfFound, page: 1, total: listOfFound.length },
+      null,
+      "Get Car List Successfully!"
+    );
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  //in real project you will getting id from req. For updating and deleting, it is recommended for you to use unique identifier such as _id to avoid duplication
+  //you will also get updateInfo from req
+  // empty target and info mean update nothing
+
+  const targetId = req.params.id;
+  const updateInfo = req.body;
+
+  //options allow you to modify query. e.g new true return lastest update of data
+  const options = { new: true };
+  try {
+    //mongoose query
+    const updated = await Car.findByIdAndUpdate(targetId, updateInfo, options);
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: updated },
+      null,
+      "Update Car Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  //in real project you will getting id from req. For updating and deleting, it is recommended for you to use unique identifier such as _id to avoid duplication
+
+  // empty target mean delete nothing
+  const targetId = req.params.id;
+
+  //options allow you to modify query. e.g new true return lastest update of data
+  const options = { new: true };
+  try {
+    //mongoose query
+    const updated = await Car.findByIdAndDelete(targetId, options);
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: updated },
+      null,
+      "Delete Car Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = carController;
